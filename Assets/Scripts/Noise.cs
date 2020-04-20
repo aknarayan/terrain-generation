d682947d83ee3982c4ece93eef682052a55c87e4
akman5599 <akshay.narayan5599@gmail.com>
2020-04-20 22:38:03 +0100

    Initial implementation of Perlin noise map

---
+++ Assets/Scripts/Noise.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public static class Noise {
+
+  public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale) {
+    float[,] noiseMap = new float[mapWidth, mapHeight];
+    
+    for (int i = 0; i < mapHeight; i++) {
+      for (int j = 0; j < mapWidth; j++) {
+        float sampleJ = j / scale;
+        float sampleI = i / scale;
+        
+        float perlinValue = Mathf.PerlinNoise(sampleJ, sampleI);
+
+        noiseMap[j, i] = perlinValue;
+      }
+    }
+    return noiseMap;
+  }
+
+}
