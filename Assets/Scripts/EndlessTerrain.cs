b5a11603bbc898dbcb36fa478e5ef15507a307cb
akman5599 <akshay.narayan5599@gmail.com>
2020-05-14 22:43:39 +0100

    Implemented terrain scaling

--- Assets/Scripts/EndlessTerrain.cs
+++ Assets/Scripts/EndlessTerrain.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class EndlessTerrain : MonoBehaviour {
 
+  const float scale = 5f;
+
   const float viewerMoveThresholdForChunkUpdate = 25f;
   const float squareViewerMoveThresholdForChunkUpdate = viewerMoveThresholdForChunkUpdate * viewerMoveThresholdForChunkUpdate;
 
@@ -33,7 +35,7 @@ public class EndlessTerrain : MonoBehaviour {
   }
 
   private void Update() {
-    viewerPosition = new Vector2(viewer.position.x, viewer.position.z);
+    viewerPosition = new Vector2(viewer.position.x, viewer.position.z) / scale;
     
     if ((viewerPositionOld - viewerPosition).sqrMagnitude > squareViewerMoveThresholdForChunkUpdate) {
       viewerPositionOld = viewerPosition;
@@ -95,8 +97,10 @@ public class EndlessTerrain : MonoBehaviour {
       meshFilter = meshObject.AddComponent<MeshFilter>();
       meshRenderer.material = material;
 
-      meshObject.transform.position = positionV3;
+      meshObject.transform.position = positionV3 * scale;
       meshObject.transform.parent = parent;
+      meshObject.transform.localScale = Vector3.one * scale;
+
       SetVisible(false);
 
       lodMeshes = new LODMesh[detailLevels.Length];
